68d41c8f341e3673af9e5178a9b0bb4d5da95b97
Aryan Sharma <38393886+imaryansharma@users.noreply.github.com>
2026-06-28 16:10:43 +0530

    fix
    https://imaryansharma.github.io/FilmyKhazana/home

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ command }) => ({
   base: command === 'build' ? '/FilmyKhazana/' : '/',
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules/hls.js')) return 'hls';
-          if (id.includes('node_modules/dashjs')) return 'dash';
-        },
-      },
-    },
-  },
 }));
